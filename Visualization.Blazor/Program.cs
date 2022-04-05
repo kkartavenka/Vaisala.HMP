407e1c26c827fb59e3219aeb0a90b62b05dd1207
Kostya Kartavenka <kostya.kartavenka@syft.com>
2022-04-05 16:55:42 +0200

    Update on port assignment

--- Visualization.Blazor/Program.cs
+++ Visualization.Blazor/Program.cs
@@ -11,15 +11,32 @@ namespace Visualization.Blazor
 {
     public class Program
     {
-        public static void Main(string[] args) {
-            CreateHostBuilder(args).Build().Run();
-        }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        private static int _port = 5000;
+        private const string _portArg = @"port";
+
+
+        public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
+
+        public static IHostBuilder CreateHostBuilder(string[] args) {
+            for(int i = 0; i < args.Length; i++)
+                switch (args[i]) {
+                    case _portArg:
+                        if (int.TryParse(args[i + 1], out int newPort)) {
+                            _port = newPort;
+                            i++;
+                        }
+                        break;
+                    default:
+                        Console.WriteLine($"Argument {args[i]} is not defined");
+                        break;
+                }
+
+            return Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder => {
-                    webBuilder.UseUrls("http://*:5000");
+                    webBuilder.UseUrls($"http://*:{_port}");
                     webBuilder.UseStartup<Startup>();
                 });
+        }
     }
 }
